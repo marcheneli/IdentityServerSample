ba297d9fc553ed3d000277539601edd522486f62
marcheneli <marcheneli@gmail.com>
2016-05-13 20:30:34 +0300

    Add ProtectedServer project

---
+++ ProtectedServer/Controllers/ProtectedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProtectedServer.Controllers
+{
+    [RoutePrefix("api/protected")]
+    public class ProtectedController : ApiController
+    {
+        [Route("")]
+        [Authorize]
+        public IHttpActionResult Get()
+        {
+            return Ok("Защищенная информация.");
+        }
+    }
+}
---
+++ ProtectedServer/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ProtectedServer.Startup))]
+
+namespace ProtectedServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServer3.AccessTokenValidation.IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:61684/"
+            });
+
+            app.UseWebApi(config);
+        }
+    }
+}
