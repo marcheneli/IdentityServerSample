0d8211b2fe6d55692410cf583a9def388028d303
marcheneli <marcheneli@gmail.com>
2016-05-14 11:31:20 +0300

    Add WebClient project

---
+++ WebClient/webpack.config.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+module.exports = {
+    entry: [
+      './src/index.jsx'
+    ],
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel',
+            query:
+            {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+};
